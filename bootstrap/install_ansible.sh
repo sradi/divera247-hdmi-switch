1d9c9479c66ee336e87a57216df4e003751fe9be
Stefan Rademacher <s.rademacher@live.de>
2020-06-14 22:37:18 +0200

    remove apt workaround ForceIPv4

--- bootstrap/install_ansible.sh
+++ bootstrap/install_ansible.sh
@@ -6,5 +6,5 @@ export ansibleSourceListEntry="deb http://ppa.launchpad.net/ansible/ansible/ubun
 grep -qxF "${ansibleSourceListEntry}" /etc/apt/sources.list || echo "${ansibleSourceListEntry}" >> /etc/apt/sources.list
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt -o Acquire::ForceIPv4=true update
-apt -o Acquire::ForceIPv4=true install ansible
+apt update
+apt install ansible
