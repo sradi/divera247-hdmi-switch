9df6018e1e1c0a6d6d005e363a8a06c278f0e42d
Stefan Rademacher <s.rademacher@live.de>
2020-06-17 20:29:25 +0200

    Add daemon script

---
+++ divera-hdmi-switch.py
@@ -0,0 +1,40 @@
+#! /usr/local/bin/python3
+
+import time
+import daemon
+import lockfile
+import sys
+import signal
+from pathlib import Path
+
+lockfile_path = '/Users/sradi/divera.pid'
+pidfile = Path(lockfile_path)
+
+def dummy():
+    print("bla")
+
+def main():
+    print(pidfile.as_posix)
+    if pidfile.exists:
+        print("Daemon is already running... exiting.")
+        sys.exit(0)
+    print("Starting Divera Daemon...")
+    while True:
+        time.sleep(180)
+        dummy()
+
+
+def shutdown(signum, frame):  # signum and frame are mandatory
+    sys.exit(0)
+
+#https://dpbl.wordpress.com/2017/02/12/a-tutorial-on-python-daemon/
+#chroot_directory=None,
+#working_directory='/var/lib/divera'
+with daemon.DaemonContext(stdout=sys.stdout,
+                          stderr=sys.stderr,
+                          pidfile=lockfile.FileLock(lockfile_path),
+                          signal_map={
+                              signal.SIGTERM: shutdown,
+                              signal.SIGTSTP: shutdown
+                          }):
+    main()
