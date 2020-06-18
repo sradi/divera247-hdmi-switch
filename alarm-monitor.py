b238789e799558821822b5101508c4f256dbf25e
Stefan Rademacher <s.rademacher@live.de>
2020-06-18 22:16:02 +0200

    Fix + sleep 120s

--- alarm-monitor.py
+++ alarm-monitor.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python
 
+import os
 import time
 import configparser
 import divera
 import hdmi_cec
 
+os.system("DISPLAY=:0 chromium-browser --kiosk www.heise.de &")
+
 config = configparser.ConfigParser()
-config.read('/home/divera/divera.ini')
+config.read('/home/pi/divera.ini')
 
 #Testeinheit
 #ACCESS_KEY = "Cm81ukIkHhutHHZ42qC5Tz3cROZOqq3_BB9f0t3e8Cgr1Hry61k3MrO2lIvQjMBX"
@@ -16,12 +19,10 @@ ACCESS_KEY=config['Divera']['accessKey']
 einheit = divera.Divera(ACCESS_KEY)
 hdmiDevice0 = hdmi_cec.HdmiCec(0)
 
-hasOpenAlarms = einheit.has_open_alarms()
-print("Open alarms? ", hasOpenAlarms)
-
 while True:
+    hasOpenAlarms = einheit.has_open_alarms()
     if hasOpenAlarms:
         hdmiDevice0.on()
     else:
         hdmiDevice0.standby()
-    time.sleep(180)
+    time.sleep(120)
