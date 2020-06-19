3d54d10089c625c37d70484e1b6a01f96a12b71f
Stefan Rademacher <s.rademacher@live.de>
2020-06-19 22:18:43 +0200

    call cec-util only on state CHANGE

--- hdmi_cec.py
+++ hdmi_cec.py
@@ -6,14 +6,23 @@ class HdmiCec:
 
     def __init__(self, deviceNo):
         self.deviceNo = deviceNo
+        self.lastCommand = ""
         os.system("echo 'scan' | cec-client -s -d 1")
 
     def on(self):
+        if self.lastCommand == "on":
+            print "device ", self.deviceNo, " already on"
+            return
+        self.lastCommand = "on"
         on_command = "echo 'on %d' | cec-client -s -d 1" % self.deviceNo
         print("Executing... ", on_command)
         os.system(on_command)
 
     def standby(self):
+        if self.lastCommand == "standby":
+            print "device ", self.deviceNo, " already in standby"
+            return
+        self.lastCommand = "standby"
         standby_command = "echo 'standby %d' | cec-client -s -d 1" % self.deviceNo
         print("Executing...", standby_command)
         os.system(standby_command)
