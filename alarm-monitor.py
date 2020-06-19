d20ca1257c230a53a94cfa693d4c40f42f6c5176
Stefan Rademacher <s.rademacher@live.de>
2020-06-19 22:19:34 +0200

    url -> divera monitor

--- alarm-monitor.py
+++ alarm-monitor.py
@@ -6,14 +6,13 @@ import configparser
 import divera
 import hdmi_cec
 
-os.system("DISPLAY=:0 chromium-browser --kiosk www.heise.de &")
+os.system("DISPLAY=:0 chromium-browser --start-fullscreen https://www.divera247.com/monitor/3.html &")
 
 config = configparser.ConfigParser()
 config.read('/home/pi/divera.ini')
 
 #Testeinheit
 #ACCESS_KEY = "Cm81ukIkHhutHHZ42qC5Tz3cROZOqq3_BB9f0t3e8Cgr1Hry61k3MrO2lIvQjMBX"
-#FF Bispinen Ortswehr Bispingen
 ACCESS_KEY=config['Divera']['accessKey']
 
 einheit = divera.Divera(ACCESS_KEY)
